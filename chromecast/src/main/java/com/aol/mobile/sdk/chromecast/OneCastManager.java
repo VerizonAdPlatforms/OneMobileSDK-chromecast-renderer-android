c80060965cfc7fcddf77d41a93ad573fa52ad5e9
AleksanderLevadniy <aleksander.levadniy@oath.com>
2018-03-15 15:26:20 +0200

    Copyright update

--- chromecast/src/main/java/com/aol/mobile/sdk/chromecast/OneCastManager.java
+++ chromecast/src/main/java/com/aol/mobile/sdk/chromecast/OneCastManager.java
@@ -1,21 +1,6 @@
 /*
- * Copyright (c) 2017. Oath.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
- * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright 2018, Oath Inc.
+ * Licensed under the terms of the MIT License. See LICENSE.md file in project root for terms.
  */
 
 package com.aol.mobile.sdk.chromecast;
@@ -66,6 +51,10 @@ public class OneCastManager {
         background.addState(new int[]{-android.R.attr.state_enabled}, disabled);
         background.addState(StateSet.WILD_CARD, drawable);
         return background;
+    }
+
+    public static final void stopCasting(Context context) {
+        CastContext.getSharedInstance(context).getSessionManager().endCurrentSession(true);
     }
 
     public void addCastButtonListener(@NonNull Context context, final CastButtonListener listener) {
@@ -129,10 +118,6 @@ public class OneCastManager {
         void enableCast();
 
         void disableCast();
-    }
-
-    public static final void stopCasting(Context context) {
-        CastContext.getSharedInstance(context).getSessionManager().endCurrentSession(true);
     }
 
 }
